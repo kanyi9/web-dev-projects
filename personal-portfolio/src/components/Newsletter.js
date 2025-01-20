798fad74254c6694fca88cf0951254b59cc5ee49
Peter <peterkanyi19193@gmail.com>
2025-01-20 10:32:49 +0300

    adds clean code

--- personal-portfolio/src/components/Newsletter.js
+++ personal-portfolio/src/components/Newsletter.js
@@ -1,20 +1,4 @@
-// import { Col, Row } from "react-bootstrap";
 
-
-// export const Newsletter = () => {
-//   return (
-//       <Col lg={12}>
-//         <div className="newsletter-bx wow slideInUp">
-//           <Row>
-//             <Col lg={12} md={6} xl={5}>
-//               <h3 className="custom-heading">Thanks for Visiting My Portfolio!</h3>
-//               <p className="custom-text">Feel free to explore my projects and get in touch if you'd like to collaborate.</p>
-//             </Col>
-//           </Row>
-//         </div>
-//       </Col>
-//   )
-// }
 
 import { Col, Row } from "react-bootstrap";
 
